0b187f4b71b0cd4ee55b5fa59f679885f0608258
dhuyartmaker <duchuy2411itd@gmail.com>
2023-08-05 09:42:27 +0700

    page support

---
+++ src/components/Input/TextAreaV1.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+export interface TextAreaProps {
+    label: string | ReactNode;
+    onChange: (e: any) => void;
+    id?: string;
+    value: string;
+}
+
+export default function TextAreaV1({ id = "id", label, onChange, value }: TextAreaProps) {
+    return (
+        <div className="mt-5">
+            <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900">
+                {label}
+            </label>
+            <textarea
+                id={id} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder=""
+                onChange={onChange}
+                value={value}
+            ></textarea>
+        </div>
+    )
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { MainLayout } from '../components/Layout'
 import Registration from './Registration/Registration'
+import Support from './Support'
 
 function HomePage() {
   return (
     <MainLayout>
       {/* <div className="h-96">Home Page</div> */}
-      <Registration />
+      {/* <Registration /> */}
+      <Support />
     </MainLayout>
   )
 }
