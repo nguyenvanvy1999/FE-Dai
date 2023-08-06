82e3e65a8e4c782702e7fa255b66c0180792e402
kentran <s2kensever@gmail.com>
2023-08-06 23:57:23 +0700

    default sales

--- src/pages/Stall/Stall.tsx
+++ src/pages/Stall/Stall.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import SideBar from './SideBar'
 import Header from './StallComponents/Header'
+import Sales from './StallComponents/Sales'
 
 const Stall = () => {
-  const [currentComponent, setCurrentComponent] = useState()
+  const [currentComponent, setCurrentComponent] = useState(<Sales />)
   return (
-    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+    <div className="flex flex-row bg-[#c9cfd6] h-screen w-screen overflow-hidden">
       <SideBar setCurrentComponent={setCurrentComponent} />
-      <div className="p-4">
+      <div className="w-full">
         <Header />
         {currentComponent}
       </div>
--- src/pages/Stall/StallComponents/Sales.tsx
+++ src/pages/Stall/StallComponents/Sales.tsx
@@ -1,5 +1,5 @@
 function Sales() {
-  return <div>Sales</div>
+  return <div className="ml-4 mt-4 bg-[#fafafa] h-full">Sales</div>
 }
 
 export default Sales
