53ba187a168bb39582ff42e8418436aa11247355
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-08-16 12:09:14 +0700

    update ui evaluation shop admin

--- src/pages/admin/pages/ShopEvalutionPage/ManagerShopEvalutionPage.tsx
+++ src/pages/admin/pages/ShopEvalutionPage/ManagerShopEvalutionPage.tsx
@@ -1,7 +1,67 @@
 import AdminLayout from '../../../../Layout/AdminLayout'
-
-export interface ManagerShopEvalutionProps {}
+import { Button, Table } from 'antd'
+export interface ManagerReviewShopPageProps {}
 
 export default function ManagerShopEvalution() {
-  return <AdminLayout>ManagerShopEvalution</AdminLayout>
+  // const [isOpenModalAddStall, setIsOpenModalAddStall] = useState(false)
+  const columns = [
+    {
+      title: 'Tên cửa hàng',
+      dataIndex: 'shopId',
+      key: 'shopId',
+    },
+    {
+      title: 'Nội dung ',
+      dataIndex: 'content',
+      key: 'content',
+    },
+    {
+      title: 'Điểm đánh giá',
+      dataIndex: 'reviewPoint',
+      key: 'reviewPoint',
+    },
+    {
+      title: 'Ngày tạo',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+    },
+    {
+      title: 'Ngày cập nhật',
+      dataIndex: 'updatedAt',
+      key: 'updatedAt',
+    },
+    {
+      title: 'Tạo bởi',
+      dataIndex: 'createdBy',
+      key: 'createdBy',
+    },
+  ]
+  return (
+    <AdminLayout>
+      <div className="p-3 max-w-full bg-[#ffff] pl-2">
+        <div className="flex items-center justify-between">
+          <p className="text-md	font-bold	mb-2 mt-3">Evaluation shop</p>
+          <div>
+            <Button
+              className="bg-red-600 mr-3 text-white"
+              // onClick={() => setIsOpenModalAddStall(true)}
+            >
+              Thùng rác
+            </Button>
+            <Button
+              className="bg-blue-700 mr-3 text-white"
+              // onClick={() => setIsOpenModalAddStall(true)}
+            >
+              Thêm mới
+            </Button>
+          </div>
+        </div>
+        <Table dataSource={[]} columns={columns} />
+        {/* <ModalAddStall
+          isOpenModalAddStall={isOpenModalAddStall}
+          setIsOpenModalAddStall={setIsOpenModalAddStall}
+        /> */}
+      </div>
+    </AdminLayout>
+  )
 }
